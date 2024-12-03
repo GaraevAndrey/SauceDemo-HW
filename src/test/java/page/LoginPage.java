82b37ede008580cf336eff65454563ce9a68f0d8
GaraevAndrey <garaevandrey@mail.ru>
2024-12-03 19:03:19 +0400

    New

--- src/test/java/page/LoginPage.java
+++ src/test/java/page/LoginPage.java
@@ -1,5 +1,6 @@
 package page;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -17,38 +18,49 @@ public class LoginPage extends BasePage {
         super(browser);
     }
 
+    @Step("Открытие браузера")
     public void open() {
         browser.get(BASE_URL);
     }
 
+    @Step("Вводим данные {user} и {password}")
     public void login(String user, String password) {
         fillUserInput(user);
         fillPasswordInput(password);
         clickSubmitBtn();
     }
 
+    //@Step("Вводим логин")
     public void fillUserInput(String user) {
         browser.findElement(USERNAME_INPUT).sendKeys(user);
     }
 
+    //@Step("ВВодим пароль")
     public void fillPasswordInput(String password) {
         browser.findElement(PASSWORD_INPUT).sendKeys(password);
     }
 
+    @Step("Кликаем на кнопку логин")
     public void clickSubmitBtn() {
         browser.findElement(LOGIN_BUTTON).click();
     }
+
+    @Step("Получаем текст из сообщения об ошибке")
     public String getERROR() {
         return browser.findElement(ERROR).getText();
     }
 
+    @Step("Получаем текст из сообщения об ошибке")
     public String getERROR2() {
         return browser.findElement(ERROR2).getText();
     }
 
+    @Step("Получаем текст из сообщения об ошибке")
     public String getERROR3() {
         return browser.findElement(ERROR3).getText();
     }
+
+    @Step("Получаем текст из сообщения об ошибке")
     public String getERROR4() {
         return browser.findElement(ERROR4).getText();
     }
